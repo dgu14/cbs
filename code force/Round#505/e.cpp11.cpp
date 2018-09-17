bc48923a96363f489f76834c0d39a86fa4f0579e
cbs0615 <ddolgu14@naver.com>
2018-09-17 22:06:23 +0900

    2018/09/17
    asdf

---
+++ code force/Round#505/e.cpp11.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll=long long;
+using dbl=double;
+using ii=pair<int,int>;
+using lii=pair<ll,ll>;
+template <class T> using V=vector<T>;
+
+#define INF                         (int)1e9
+#define EPS                         1e-9
+#define LONG_INF                    (1ll << 63 - 1ll)
+#define bitcount                    __builtin_popcount
+#define gcd                         __gcd
+#define forn(i,n) 					for(int i=0;i<(int)n;i++)
+#define forn1(i,n)					for(int i=1;i<=(int)n;i++)
+
+///////////////////////////////////////////////////////////////////////
+
+#define TESTING
+int main()
+{
+    #ifdef TESTING
+        int n;
+        ofstream fout("a.out");
+        ifstream fin("a.in");
+
+        fin>>n;
+        fout << n << endl;
+    #else
+
+
+    #endif
+
+	return 0;
+}
