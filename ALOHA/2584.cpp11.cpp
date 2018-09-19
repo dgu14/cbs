17a14bf51d77554bf74ecb1e05e975f94797acfd
cbs0615 <ddolgu14@naver.com>
2018-09-19 14:08:47 +0900

    2018/09/19
    asdf

--- ALOHA/2584.cpp11.cpp
+++ ALOHA/2584.cpp11.cpp
@@ -17,51 +17,108 @@ template <class T> using V=vector<T>;
 #define rforn(i,n)                  for(int i=(int)n-1;i>=0;i--)
 #define rforn1(i,n)                 for(int i=(int)n;i>=1;i--)
 
-/**
-    cbs0615/ddolgu14
-    AOJ BOJ CODEFORCE CODEGROUND
-*/
-
-int n,k, sz[1050];
-V<V<ii>> adj[1050];
-int dp[1050][1050];
-
-int dfs(int src, V<bool>& vst)
+int n,k, adj[1050][1050], sz[1050], dp[1050][1050][2], tdp[1050][2];
+V<int> sel[1005][1005];
+V<int> selected;
+void mrg(int u, int v)
 {
-    int ret=1;
-    vst[src]=true;
+    forn(i,sz[u]+sz[v]) tdp[i][0]=tdp[i][1]=INF;
 
-    for(ii there:adj[src])
+    int pv, pj;
+    forn(i,sz[u])
     {
-        if(!vst[there.first])
+        forn(j,sz[v])
         {
-            ret+=dfs(there.first, vst);
+            forn(q, 2)
+            {
+                if(tdp[i+j+(q==0)][0] > dp[u][i][0]+dp[v][j][q])
+                {
+                    tdp[i+j+(q==0)][0]=dp[u][i][0]+dp[v][j][q]);
+                    pv=v; pj=j;
+                }
+            }
         }
     }
 
-    forn(i, ret+1)
+    sel[u][i+j+(q==0)][0].push_back({v,j});
+
+    forn(i,sz[u])
     {
-        forn ggggggggggggg
+        forn(j,sz[v])
+        {
+            forn(q,2)
+            {
+                if(tdp[i+j+(q==0)][1] > dp[u][i][1]+dp[v][j][q]+(q==1?adj[u][v]:0))
+                {
+                    tdp[i+j+(q==0)][1] = dp[u][i][1]+dp[v][j][q]+(q==1?adj[u][v]:0);
+                    pv=v; pj=j;
+                }
+            }
+        }
     }
 
-    return ret;
+    sel[u][i+j+(q==0)][1].push_back({v,j});
+
+    forn(i,sz[u]+sz[v]) dp[u][i][0]=tdp[i][0], dp[u][i][1]=tdp[i][1];
+}
+
+void dfs(int src, V<bool>& vst)
+{
+    vst[src]=true; sz[src]++; dp[src][0][0]=0; dp[src][0][1]=0;
+
+    forn1(dst, n)
+    {
+        if(adj[src][dst]>0 && !vst[dst])
+        {
+            dfs(dst, vst); mrg(src, dst); sz[src]+=sz[dst];
+        }
+    }
+}
+
+void print(int src, int max_val, V<bool>& vst)
+{
+    // 시작은 루트값이 선택됬나 안됬냐를 알수있다.
+    //
+
+    forn1(dst, n)
+    {
+        if(adj[src][dst]>0 && !vst[src])
+        {
+
+        }
+    }
 }
 
 int main()
 {
     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    cin>>n>>k;
+    cin>>n>>k; int a,b,c;
+    forn(i,n-1) cin>>a>>b>>c, adj[a][b]=c, adj[b][a]=c;
 
-    int a,b,c;
-    forn(i,n)
-    {
-        cin>>a>>b>>c;
-        adj[a].push_back({b,c});
-        adj[b].push_back({a,c});
-    }
+    V<bool> vst(1050, false);
+    dfs(1, vst);
 
-    V<bool> vst(n+1, false);
+    cout << min(dp[1][k-1][0], dp[1][k][1]) << endl;
+
+
 
 	return 0;
 }
 
+/**
+6 2
+1 2 1
+1 4 1
+1 3 2
+3 5 1
+3 6 2
+
+
+7 2
+1 2 10
+1 3 15
+1 4 30
+5 3 20
+6 3 10
+4 7 25
+*/
--- BOJ/dp/2197.cpp11.cpp
+++ BOJ/dp/2197.cpp11.cpp
@@ -17,12 +17,52 @@ template <class T> using V=vector<T>;
 #define rforn(i,n)                  for(int i=(int)n-1;i>=0;i--)
 #define rforn1(i,n)                 for(int i=(int)n;i>=1;i--)
 
-int n,m;
+int n,m, sz[1000], dp[1000][1000], tdp[1000];
+V<int> adj[1000];
 
+void mrg(int u, int v)
+{
+    forn(i,sz[u]+sz[v]) { if(i<sz[u]) tdp[i]=dp[u][i]+1; else tdp[i]=INF; }
+
+    forn(i,sz[u])
+    {
+        forn(j,sz[v])
+        {
+            tdp[i+j+1]=min(tdp[i+j+1], dp[u][i]+dp[v][j]);
+        }
+    }
+
+    forn(i,sz[u]+sz[v]) dp[u][i]=tdp[i];
+}
+
+void dfs(int src, V<bool>& vst)
+{
+    vst[src]=true; dp[src][0]=0; sz[src]++;
+
+    for(int there:adj[src])
+    {
+        if(!vst[there]) dfs(there, vst), mrg(src, there), sz[src]+=sz[there];
+    }
+}
 
 int main()
 {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    cin>>n>>m; int a,b;
+    forn(i,n-1) cin>>a>>b, adj[a].push_back(b), adj[b].push_back(a);
 
+    forn(i,1000) forn(j,1000) dp[i][j]=INF;
+    V<bool> vst(1000, false); dfs(1, vst);
+
+    int min_c=INF;
+    forn1(i,n)
+    {
+        if(i==1) min_c=min(min_c, dp[1][m-1]);
+        else min_c=min(min_c, dp[i][m-1]+1);
+    }
+
+    cout << min_c << endl;
 
 	return 0;
 }
+
