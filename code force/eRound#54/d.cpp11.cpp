2ce88e3b18e9fc4dcb77c23bfae5acbbb08c35c1
cbs0615 <36907192+dgu14@users.noreply.github.com>
2018-11-15 17:40:41 +0900

    asdf
    asdf

--- code force/eRound#54/d.cpp11.cpp
+++ code force/eRound#54/d.cpp11.cpp
@@ -4,6 +4,7 @@ template <class T> using V=vector<T>;
 using ll=long long;
 using dbl=double;
 using ii=pair<int,int>;
+using lii=pair<ll,int>;
 #define INF                         (int)1e9
 #define EPS                         1e-9
 #define LONG_INF                    (1ll << 63 - 1ll)
@@ -19,7 +20,8 @@ using ii=pair<int,int>;
 int n,m,k,x,y,w;
 V<ii> adj[300005];
 int ddalin[300005], bad[300005];
-V<int> parent[300005];
+int parent[300005];
+map<ii, int> mp;
 
 int main()
 {
@@ -34,24 +36,26 @@ int main()
 
         adj[x].push_back({y,w});
         adj[y].push_back({x,w});
+
+        mp[{x,y}]=mp[{y,x}]=i;
     }
 
-    priority_queue<ii, V<ii>, greater<ii>> pq;
+    priority_queue<lii, V<lii>, greater<lii>> pq;
     V<bool> vst(n, false);
-    V<int> dist(n, INF);
-    pq.push({0, 0});
+    V<ll> dist(n, LONG_INF);
+    pq.push({0L, 0});
 
     while(pq.size())
     {
-        ii here=pq.top(); pq.pop();
+        lii here=pq.top(); pq.pop();
         if(dist[here.second]<here.first) continue;
         dist[here.second]=here.first;
         vst[here.second]=true;
 
         for(int i=0;i<adj[here.second].size();i++)
         {
-            int there=adj[here.second][i].first;
-            int cost=adj[here.second][i].second;
+            ll there=adj[here.second][i].first;
+            ll cost=adj[here.second][i].second;
 
             if(!vst[there] && dist[there]>dist[here.second]+cost)
             {
@@ -60,22 +64,23 @@ int main()
         }
     }
 
-    // bfs 트리 구성
-
-    // 트리 노드가 아닌거는 k--
-    int s=0;
+    int s=0, d=0;
+    fill(parent, parent+300005, -1);
     forn(i,n)
     {
         for(int j=0;j<adj[i].size();j++)
         {
-            int there=adj[i][j].first;
-            int cost=adj[i][j].second;
+            ll there=adj[i][j].first;
+            ll cost=adj[i][j].second;
 
-            // 최단거리인 경우
             if(dist[i]==dist[there]+cost)
             {
-                ddalin[there]++; // 딸린거 하나 플러스.
-                parent[i].push_back(there);
+                if(parent[i]==-1)
+                {
+                    parent[i]=there;
+                    ddalin[there]++;
+                }
+                else d++;
             }
             else if(dist[there]==dist[i]+cost);
             else s++;
@@ -83,43 +88,66 @@ int main()
     }
 
     k-=s/2;
+    k-=d;
+    set<int> notIncluded;
+    if(k<0)
+    {
+        forn(i,n)
+        {
+            forn(j,adj[i].size())
+            {
+                ll there=adj[i][j].first;
+                ll cost=adj[i][j].second;
 
-    // 지우는 거는 어케 하냐
-    // 딸린거 없는것부터 지우는데, 그 중에서도 parent갯수가 많은거부터 지운다.
+                if(there!=parent[i] && parent[there]!=i)
+                {
+                    notIncluded.insert(mp[{i, there}]);
+
+                    if(notIncluded.size()+k==0) break;
+                }
+            }
+
+            if(notIncluded.size()+k==0) break;
+        }
+    }
+
+    queue<ll> qq;
 
     forn(i,n)
     {
-        pq.push({ddalin[i], i});
+        if(ddalin[i]==0) qq.push(i);
     }
 
-    while(pq.size() && k)
+    while(k>0 && qq.size())
     {
-        ii now=pq.top(); pq.pop();
+        ll tt=qq.front(); qq.pop();
 
-        if(parent[now.second].size()>k)
+        ddalin[parent[tt]]--; k--;
+        bad[tt]=true;
+
+        if(ddalin[parent[tt]]==0)
         {
-            k=0;
-        }
-        else
-        {
-            bad[now.second]=true;
-            k-=parent[now.second].size();
+            qq.push(parent[tt]);
         }
     }
 
-    V<int> goods;
+    V<ll> goods;
     forn(i,n)
     {
-        if(!bad[i]) goods.push_back(i);
+        if(!bad[i] && i!=0)
+        {
+            goods.push_back(mp[{i, parent[i]}]);
+        }
     }
 
-    cout << goods.size() << '\n';
-    for(int i=0;i<goods.size();i++)
+    for(auto it=notIncluded.begin();it!=notIncluded.end();it++)
     {
-        cout << goods[i]+1 << ' ';
+        goods.push_back(*it);
     }
-    cout << '\n';
+
+    cout << goods.size() << endl;
+    forn(i, goods.size()) cout << goods[i]+1 << ' ';
+    cout <<endl;
 
 	return 0;
 }
-
